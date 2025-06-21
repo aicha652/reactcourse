2322e54aefa702d68ed6cc3d0e6aeb7137a153cc
aicha652 <aichaazlf652@gmail.com>
2025-06-21 12:34:11 +0200

    checkbox with state

--- usestatehook/src/MyForm.js
+++ usestatehook/src/MyForm.js
@@ -2,7 +2,16 @@ import { useState } from "react";
 
 export default function MyForm() {
 
-    const [InputForms, setInputForms] = useState({Name: "", Email: ""})
+    const [InputForms, setInputForms] = useState(
+        {Name: "",
+         Email: "", 
+         GeneralInfo: "",
+         isStudent: false
+        })
+    
+        function handleCheckBoxChange(event) {
+            setInputForms({...InputForms, isStudent: event.target.checked})
+        }
 
     return(
         <form
@@ -12,6 +21,7 @@ export default function MyForm() {
         }
         }
         style={ { paddingTop: "30px" } }>
+
             <label>Name: </label>
             <input
             value={InputForms.Name}
@@ -21,11 +31,30 @@ export default function MyForm() {
             />
             <br />
             <br />
+
             <label>Email: </label>
             <input 
             value={InputForms.Email}
             onChange={(event) => {setInputForms({...InputForms, Email: event.target.value})} }
             />
+            <br />
+            <br />
+            <label>Are you a student</label>
+            <input 
+            type="checkbox"
+            checked={InputForms.isStudent}
+            onChange={handleCheckBoxChange}
+            />
+            <br />
+            <br />
+            <label>General Info: </label>
+            <textArea 
+            value={InputForms.GeneralInfo} 
+            onChange={(event)  =>{
+                setInputForms({...InputForms, GeneralInfo: event.target.value})
+            }}
+            />
+
             <br />
             <br />
             <button>Submit</button>
