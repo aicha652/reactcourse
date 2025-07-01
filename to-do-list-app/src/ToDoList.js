9461c4c45cfcf464c00e6c67ad43fd3f7b673c7d
aicha652 <aichaazlf652@gmail.com>
2025-07-01 14:35:55 +0200

    all buttons work

--- to-do-list-app/src/ToDoList.js
+++ to-do-list-app/src/ToDoList.js
@@ -10,12 +10,14 @@ export default function ToDoList() {
     const[tasks, setTasks] = useState([])
     const[done, setDone] = useState(false)
     const[index, setIndex] = useState(0)
+    const[filteredTasks, setFilteredTasks] = useState([])
 
     function handleAddClick() {
         console.log(tasks)
         if(taskInput.trim() !== ""){
             let newTask = {name: taskInput, id: index, isDone: done}
             setTasks([...tasks, newTask])
+            setFilteredTasks([...tasks, newTask])
         }
         setIndex(index + 1)
     }
@@ -25,7 +27,8 @@ export default function ToDoList() {
         const filterTasks = tasks.filter(task =>
             task.id !== taskId
         )
-        setTasks([...filterTasks])
+        setTasks(filterTasks);
+        setFilteredTasks(filterTasks)
     }
     
     function handleUpdateClick(taskId) {
@@ -55,44 +58,83 @@ export default function ToDoList() {
             return task
         })
         setTasks(updateDoneTask)
+        setFilteredTasks(updateDoneTask)
     }
 
-    const allTasks = tasks.map((task) =>{
-        return(
-                <div className="card" key={task.id}>
-                    <h4>{task.name}</h4>
-                    <div className="actions">
-                        <AiOutlineDelete
-                         onClick={()=>{handleDeleteClick(task.id)}} 
-                         style={{borderRadius: "50%", color: "red", 
-                                 background: "#fff", border:"1px solid red",
-                                 fontSize: "25px"}}
-                        />
-                        <MdOutlineEdit 
-                         onClick={()=>{handleUpdateClick(task.id)}}
-                         style={{borderRadius: "50%", color: "blue", 
-                                 background: "#fff", border:"1px solid blue",
-                                 fontSize: "25px"}}
-                        />
-                        <MdDone
-                         onClick={() => {handleDoneTaskClick(task.id)}}
-                         className={task.isDone ? "task-done" : "task-notdone" }
-                         />
-                    </div>
-                </div>)
-    })
+    function allTypes() {
+        setFilteredTasks(tasks)
+    }
+
+    function handleDoneTasks(){
+        const doneTasks = tasks.filter(task=>
+            task.isDone == true
+        )
+        setFilteredTasks(doneTasks)
+    }
+
+    function handleNotDoneTasks() {
+        const notDoneTasks = tasks.filter(task=>
+            task.isDone == false
+        )
+        setFilteredTasks(notDoneTasks)
+    }
+
 
     return(
         <div className="container">
             <p>مهامي</p>
             <hr />
             <div className="arrayTitles">
-                <button className="column">الكل</button>
-                <button className="column">منجز</button>
-                <button className="column">غير منجز</button>
+                <button 
+                 onClick={() =>{
+                    allTypes()
+                 }}
+                 className="column"
+                 >الكل
+                </button>
+                <button 
+                 onClick={() =>{
+                    handleDoneTasks()
+                 }}
+                 className="column"
+                 >منجز
+                 </button>
+                <button
+                 onClick={() => {
+                    handleNotDoneTasks()
+                 }}
+                 className="column"
+                 >غير منجز
+                 </button>
             </div>
             <div>
-                {tasks.length > 0 ? allTasks : <></>}
+                {filteredTasks.length > 0 ?  
+                  filteredTasks.map((task) =>{
+                    return(
+                           <div className="card" key={task.id}>
+                           <h4>{task.name}</h4>
+                           <div className="actions">
+                            <AiOutlineDelete
+                             onClick={()=>{handleDeleteClick(task.id)}} 
+                             style={{borderRadius: "50%", color: "red", 
+                                 background: "#fff", border:"1px solid red",
+                                 fontSize: "25px"}}
+                            />
+                            <MdOutlineEdit 
+                             onClick={()=>{handleUpdateClick(task.id)}}
+                             style={{borderRadius: "50%", color: "blue", 
+                                 background: "#fff", border:"1px solid blue",
+                                 fontSize: "25px"}}
+                            />
+                            <MdDone
+                             onClick={() => {handleDoneTaskClick(task.id)}}
+                             className={task.isDone ? "task-done" : "task-notdone" }
+                            />
+                            </div>
+                            </div>)
+                         })
+                    : 
+                  <></>}
             </div>
             <div className="bottomList" >
                 <button
